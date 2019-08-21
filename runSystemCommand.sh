74fea7f2203d88c471eadbc2f7348374ec8231d6
amckenzie <allan.mckenzie@agilesphere.co.uk>
2019-08-21 16:08:14 +0100

    Improve runSystemCommand.sh script

--- runSystemCommand.sh
+++ runSystemCommand.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bash
 
-FRAMEWORK_JMX_COMMAND_CLIENT_VERSION=1.2.0-SNAPSHOT
+################################################
+# Usage:
+#
+# To run a command (e.g. CATCHUP):
+#   ./runSystemCommand.sh <command name>
+#
+# To list all commands:
+#   ./runSystemCommand.sh
+#
+################################################
+
+FRAMEWORK_JMX_COMMAND_CLIENT_VERSION=2.1.0=SNAPSHOT
+CONTEXT_NAME="people"
+USER_NAME="admin"
+PASSWORD="admin"
 
 #fail script on error
 set -e
-echo "Running Framework System Command Client $1 $2 $3 $4 $5 $6"
+
 echo
-java -jar target/framework-jmx-command-client-${FRAMEWORK_JMX_COMMAND_CLIENT_VERSION}.jar $1 $2 $3 $4 $5 $6
+echo "Framework System Command Client for $CONTEXT_NAME context"
+echo
+
+if [ -z "$1" ]; then
+  echo "Listing commands"
+  echo
+  java -jar target/framework-jmx-command-client-${FRAMEWORK_JMX_COMMAND_CLIENT_VERSION}.jar -l -u "$USER_NAME" -pw "$PASSWORD" -cn "$CONTEXT_NAME"
+else
+  COMMAND=$1
+  echo "Running command '$COMMAND'"
+  echo
+  java -jar target/framework-jmx-command-client-${FRAMEWORK_JMX_COMMAND_CLIENT_VERSION}.jar -c "$COMMAND" -u "$USER_NAME" -pw "$PASSWORD" -cn "$CONTEXT_NAME"
+fi
